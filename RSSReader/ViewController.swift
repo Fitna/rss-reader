6c52885c997bed9c02e8772c869e0347f6f45b22
Oleg Matveev <o.matveev@itmegastar.com>
2019-04-24 07:41:34 +0700

    create project

---
+++ RSSReader/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Akira Silverhaze on 24/04/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
