7b8b235f60b6f1b773507df89a0fe19ab09606f7
Oleg Matveev <o.matveev@itmegastar.com>
2019-04-24 09:25:15 +0700

    no message

---
+++ RSSReader/Views/RSSFeedCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RSSFeedCollectionViewCell.swift
+//  RSSReader
+//
+//  Created by Akira Silverhaze on 24/04/2019.
+//
+
+import UIKit
+
+class RSSFeedCollectionViewCell: UICollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
