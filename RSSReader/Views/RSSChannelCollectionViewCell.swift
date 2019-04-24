1db4ba98ccab6f4e0b678f07240a00c05408a774
Oleg Matveev <o.matveev@itmegastar.com>
2019-04-24 20:45:14 +0700

    no message

--- RSSReader/Views/RSSChannelCollectionViewCell.swift
+++ RSSReader/Views/RSSChannelCollectionViewCell.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class RSSChannelCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblURL: UILabel!
+    @IBOutlet weak var btnFavorites: UIButton!
+    @IBOutlet weak var btnDelete: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
 }
--- RSSReader/Views/RSSFeedCollectionViewCell.swift
+++ RSSReader/Views/RSSFeedCollectionViewCell.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 class RSSFeedCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 }
